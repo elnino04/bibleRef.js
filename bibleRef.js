60518d28b403d4df8766154daa6f1bc5f83e4c8b
Joah Gerstenberg <joahd@gbergfamily.com>
2013-12-08 09:10:39 -0600

    I can. not. believe. that I did this xD

--- bibleRef.js
+++ bibleRef.js
@@ -28,7 +28,7 @@
 		return "http://www.biblegateway.com/passage/?search="+p.replace(/\s/, '%20')+"&version="+version;
 	}
 
-	refs = document.body.innerHTML.match(/(genesis|exodus|leviticus|numbers|deuteronomy|joshua|judges|ruth|ezra|nehemiah|esther|job|psalms|proverbs|ecclesiastes|song of solomon|isaiah|jeremiah|lamentations|ezekiel|daniel|hosea|joel|amos|obadiah|jonah|micah|nahum|habakkuk|zephaniah|haggai|zechariah|malachi|matthew|mark|luke|john|acts|romans|galatians|ephesians|philippians|colossians|titus|philemon|hebrews|james|jude|revelation)\s\d+:\d+/ig).concat(document.body.innerHTML.match(/\d+\s(samuel|kings|chronicles|corinthians|thessalonians|timothy|john|peter)\s\d+:\d+/ig));
+	refs = document.body.innerHTML.match(/(genesis|exodus|leviticus|numbers|deuteronomy|joshua|judges|ruth|ezra|nehemiah|esther|job|psalm|proverbs|ecclesiastes|song of solomon|isaiah|jeremiah|lamentations|ezekiel|daniel|hosea|joel|amos|obadiah|jonah|micah|nahum|habakkuk|zephaniah|haggai|zechariah|malachi|matthew|mark|luke|john|acts|romans|galatians|ephesians|philippians|colossians|titus|philemon|hebrews|james|jude|revelation)\s\d+:\d+/ig).concat(document.body.innerHTML.match(/\d+\s(samuel|kings|chronicles|corinthians|thessalonians|timothy|john|peter)\s\d+:\d+/ig));
 	
 	for (i in refs) {
 		j = refs[i];

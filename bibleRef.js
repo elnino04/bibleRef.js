cf92b759047f2d56b4a2d4fc0ac8c0663d885e72
Joah Gerstenberg <joahd@gbergfamily.com>
2013-12-11 08:41:29 -0600

    Lots of little OCD fixes

--- bibleRef.js
+++ bibleRef.js
@@ -5,56 +5,56 @@
  */
 $(document).ready(function(){
 	// Declare all variables
-	var src, target, range, class_, version, refs, selector, esv, bg, url;
+	var _src, _target, _range, _class, _version, _refs, _selector, _esv, _bg, _url;
 
-	// Set source to default to esvbible if it is not already set to biblegateway
-	src = bibleRef && bibleRef.src === 'biblegateway' ? 'biblegateway' : 'esvbible';
+	// Set _src to default to _esvbible if it is not already set to biblegateway
+	_src = bibleRef && bibleRef.src === 'biblegateway' ? 'biblegateway' : '_esvbible';
 
-	// Set target to default to _self if not already defined
-	target = bibleRef && bibleRef.target !== '_self' ? bibleRef.target : '_self';
+	// Set _target to default to _self if not already defined
+	_target = bibleRef && bibleRef.target !== '_self' ? bibleRef.target : '_self';
 
 	// Set class to empty string if bibleRef.class is not defined
 	class_ = bibleRef && bibleRef.class ? bibleRef.class : '';
 
-	// Set range to default to body if not already defined
-	range = bibleRef && bibleRef.range !== 'body' ? bibleRef.range : 'body';
+	// Set _range to default to body if not already defined
+	_range = bibleRef && bibleRef.range !== 'body' ? bibleRef.range : 'body';
 
-	// Set version to default to NIV if not already defined
-	version = bibleRef && bibleRef.version ? bibleRef.version : 'NIV';
+	// Set _version to default to NIV if not already defined
+	_version = bibleRef && bibleRef.version ? bibleRef.version : 'NIV';
 
-	// Collect all Bible references in [range]
-	refs = (range ? document.getElementById(range) : document.body).innerHTML.match(/(\d+\s)?((I*)+\s)?(genesis|exodus|leviticus|numbers|deuteronomy|joshua|judges|samuel|kings|chronicles|ruth|ezra|nehemiah|esther|job|psalm|psalms|proverbs|ecclesiastes|song of solomon|isaiah|jeremiah|lamentations|ezekiel|daniel|hosea|joel|amos|obadiah|jonah|micah|nahum|habakkuk|zephaniah|haggai|zechariah|malachi|matthew|mark|luke|john|acts|romans|corinthians|galatians|ephesians|philippians|colossians|thessalonians|timothy|titus|philemon|hebrews|james|peter|jude|revelation)\s\d+(:\d+)?(\-\d+)?(:\d+)?((\,\s\d+)?(:\d+)?(\-\d+)?(:\d+)?)*/ig);
+	// Collect all Bible references in [_range]
+	_refs = (_range ? document.getElementById(_range) : document.body).innerHTML.match(/(I+\s)?(\d\s)?(genesis|exodus|leviticus|numbers|deuteronomy|joshua|judges|samuel|kings|chronicles|ruth|ezra|nehemiah|esther|job|psalm|psalms|proverbs|ecclesiastes|song of solomon|isaiah|jeremiah|lamentations|ezekiel|daniel|hosea|joel|amos|obadiah|jonah|micah|nahum|habakkuk|zephaniah|haggai|zechariah|malachi|matthew|mark|luke|john|acts|romans|corinthians|galatians|ephesians|philippians|colossians|thessalonians|timothy|titus|philemon|hebrews|james|peter|jude|revelation)\s\d+(:\d+)?(\-\d+)?(:\d+)?((\,\s\d+)?(:\d+)?(\-\d+)?(:\d+)?)*/ig);
 	
-	// Set selector to default to body if range is not defined.
-	selector = range ? '#' + range : 'body';
+	// Set _selector to default to body if _range is not defined.
+	_selector = _range ? '#' + _range : 'body';
 
-	// Set esv to function returning esvbible.org Bible ref URL
-	esv = function(p) {
-		return "http://www.esvbible.org/" + p.replace(/\s/g, '+');
+	// Set _esv to function returning _esvbible.org Bible ref URL
+	_esv = function(_p) {
+		return "http://www.esvbible.org/" + _p.replace(/\s/g, '+');
 	}
 
-	// Set bg to function returning biblegateway.com Bible ref URL
-	bg = function(p) {
-		return "http://www.biblegateway.com/passage/?search=" + p.replace(/\s/g, '%20') + "&version=" + version;
+	// Set _bg to function returning biblegateway.com Bible ref URL
+	_bg = function(_p) {
+		return "http://www.biblegateway.com/passage/?search=" + _p.replace(/\s/g, '%20') + "&_version=" + _version;
 	}
 
-	// Set url to return appropriate url according to src setting
-	url = function(p) {
-		return src === 'esv' ? esv(p) : src === 'biblegateway' ? bg(p) : esv(p);
+	// Set _url to return appropriate _url according to _src setting
+	_url = function(_p) {
+		return _src === '_esv' ? _esv(_p) : _src === 'biblegateway' ? _bg(_p) : _esv(_p);
 	}
 
-	// Loop through refs
-	for (i in refs) {
-		j = refs[i];
+	// Loop through _refs
+	for (_i in _refs) {
+		_j = _refs[_i];
 
 		// If ref is not undefined
-		if (j) {
+		if (_j) {
 
 			// Create a new Regex to check for another trailing reference 
-			k = new RegExp(j + "(?!\\,\\s\\d+(:\\d+)?)", "g");
+			k = new RegExp(_j + "(?!\\,\\s\\d+(:\\d+)?)", "g");
 
 			// Replaces all occurrences of the reference with the appropriate link
-			$(selector).html($(selector).html().replace(k, "<a data-bible-ref href='" + url(j) + "' target=" + target + " class='" + class_ + "'>" + j + "</a>"));
+			$(_selector).html($(_selector).html().replace(k, "<a data-bible-ref href='" + _url(_j) + "' target=" + _target + " class='" + _class + "'>" + _j + "</a>"));
 		}
 	}
 });
